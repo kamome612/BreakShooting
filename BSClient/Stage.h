ec41d50a92c99233ee470a813fd664a6c6f8bf6e
光秀 竹田 <230645@jc-21.jp>
2025-01-10 15:09:44 +0900

    光秀追加

--- BSClient/Stage.h
+++ BSClient/Stage.h
@@ -3,5 +3,23 @@
 class Stage :
     public GameObject
 {
+private:
+	int Cr;
+public:
+	//引数：parent  親オブジェクト（SceneManager）
+	Stage(GameObject* parent);
+
+	//初期化
+	void Initialize() override;
+
+	//更新
+	void Update() override;
+
+	//描画
+	void Draw() override;
+
+	//開放
+	void Release() override;
+
 };
 
--- BSServer/Stage.cpp
+++ BSServer/Stage.cpp
@@ -1,1 +1,25 @@
 #include "Stage.h"
+Stage::Stage(GameObject* parent)
+	: GameObject(parent, "Stage")
+{
+}
+
+void Stage::Initialize()
+{
+
+}
+
+void Stage::Update()
+{
+}
+
+void Stage::Draw()
+{
+	Cr = GetColor(0, 0, 0);
+
+	DrawCircle(300, 400, 100, FALSE);
+}
+
+void Stage::Release()
+{
+}
