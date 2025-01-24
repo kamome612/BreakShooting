5a90409f2773c946f09625f4f1a6e6356cd91269
光秀 竹田 <230645@jc-21.jp>
2025-01-24 16:05:01 +0900

    ステージ変更

--- BSServer/Stage.cpp
+++ BSServer/Stage.cpp
@@ -19,11 +19,29 @@ void Stage::Update()
 void Stage::Draw()
 {
 
-	DrawBox(100, 100, 1200, 680, Cr, FALSE);
-	//DrawOval(640, 360, 550, 350, Cr, FALSE);
+
+	DrawOval(640, 360, 550, 350, Cr, FALSE);
 
 }
 
 void Stage::Release()
 {
 }
+
+//bool Stage::CollideCircle(float x, float y, float r) 
+//{
+//	//x,y,r‚ª‘Šè‚Ì‰~‚Ìî•ñ
+//	//©•ª‚Ì‰~‚Ìî•ñ
+//	float myCenterX = 320;
+//	float myCenterY = 180;
+//	float myR = 50.0f;
+//	float dx = myCenterX - x;
+//	float dy = myCenterY - y;
+//	if ((dx * dx + dy * dy) < ((r + myR) * (r + myR))) {
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+//}
+
