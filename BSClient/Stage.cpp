87d3f46bb06566861816cec5210113b970e0e798
光秀 竹田 <230645@jc-21.jp>
2025-02-07 16:36:05 +0900

    ステージの壁の追加、ステージの枠を少し太くした

--- BSClient/Stage.cpp
+++ BSClient/Stage.cpp
@@ -6,19 +6,42 @@ Stage::Stage(GameObject* parent)
 
 void Stage::Initialize()
 {
-
+	transform_.position_.x = 100;
+	transform_.position_.y = 100;
+	width_ = 1200;
+	height_ = 680;
 }
 
 void Stage::Update()
 {
+	//↓外枠の壁
 	Cr = GetColor(255, 0, 0);
 }
 
 void Stage::Draw()
 {
+	int Linelength = 60;
+	int LineHeight = 400;
+	int xSStart = 170;
+	int count = 15;
+	int Hcount = 3;
+	//↓ステージの外枠
 	DrawBoxAA(100, 100, 1200, 680, Cr, FALSE);
 	DrawBoxAA(101, 101, 1201, 681, Cr, FALSE);
-	//DrawOval(640, 360,　550,　350, Cr, FALSE);
+	
+	//↓ステージを分断する破壊できる壁
+	for (int i = 0; i < count; i++)
+	{
+		//for (int j = 0; j < count; j++)
+		//{
+			int xS = xSStart + i * (Linelength + 5);
+			int yS = LineHeight;
+			int xE = xS + Linelength;
+			int yE = LineHeight;
+			DrawLine(xS, yS, xE, yE, GetColor(255, 255, 255));
+		//}
+		
+	}
 	
 }
 
