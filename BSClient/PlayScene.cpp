11ba13bbc8623c7d12300d9a12b2a06e076eec40
琉夏 吉田 <230395@jc-21.jp>
2025-02-28 16:10:06 +0900

    またもや弾の角度変更

--- BSClient/PlayScene.cpp
+++ BSClient/PlayScene.cpp
@@ -93,7 +93,7 @@ void PlayScene::Update()
 		bPos.y += 64;
 		bPos.x -= 30;
 		pBullet->SetPosition(bPos.x, bPos.y);
-		pBullet->SetAngle(120);
+		pBullet->SetAngle(100);
 		pPlayer->SetBullets(pBullet);
 		
 		//XMFLOAT3 bulletPos = pBullet->GetPosition();
--- BSServer/PlayScene.cpp
+++ BSServer/PlayScene.cpp
@@ -91,7 +91,7 @@ void PlayScene::Update()
 		bPos.y += 64;
 		bPos.x -= 30;
 		pBullet->SetPosition(bPos.x, bPos.y);
-		pBullet->SetAngle(120);
+		pBullet->SetAngle(100);
 		pPlayer->SetBullets(pBullet);
 
 		//XMFLOAT3 bulletPos = pBullet->GetPosition();
